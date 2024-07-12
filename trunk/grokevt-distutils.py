4a9223bcc7c1fb750c9b394fdc8df06b528f923b
Emmanuel Arias <eamanu@yaerobi.com>
2024-07-12 07:15:41 -0300

    Remove deprecated Distutils
    This commit remove distutils.core.setup from source to use setuptools.setup
    instead. Distutils is deprecated and remove from Python3.12.
    
    Original bug reported here: https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1065871

--- trunk/grokevt-distutils.py
+++ trunk/grokevt-distutils.py
@@ -1,4 +1,4 @@
 # Called from make with appropriate python version
 
-from distutils.core import setup
+from setuptools import setup
 setup(name='grokevt', version='0.5', package_dir={'':'lib'}, py_modules=['grokevt'])
